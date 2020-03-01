43352232ec571a59692fb33433910b265b2721ad
SDen4 <asw325@yandex.ru>
2020-03-01 18:26:07 +0500

    first type of animation around Y

--- src/components/MenuFeedback.vue
+++ src/components/MenuFeedback.vue
@@ -5,23 +5,27 @@
         .feedback__container
             ul.feedback__list
                 li.feedback__item
-                    .feedback__text Sample
+                    .feedback__info.feedback__img_front
+                        .feedback__img_container
+                            img.feedback__user-photo(src="/assets/images/user.png")
+                    .feedback__info.feedback__text_back
+                        p Feedback text
                 li.feedback__item
-                    .feedback__text Sample
+                    .feedback__info Sample
                 li.feedback__item
-                    .feedback__text Sample
+                    .feedback__info Sample
                 li.feedback__item
-                    .feedback__text Sample
+                    .feedback__info Sample
                 li.feedback__item
-                    .feedback__text Sample
+                    .feedback__info Sample
                 li.feedback__item
-                    .feedback__text Sample
+                    .feedback__info Sample
                 li.feedback__item
-                    .feedback__text Sample
+                    .feedback__info Sample
                 li.feedback__item
-                    .feedback__text Sample
+                    .feedback__info Sample
                 li.feedback__item
-                    .feedback__text Sample
+                    .feedback__info Sample
 </template>
 
 <script>
